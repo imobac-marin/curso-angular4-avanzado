00f7536a4c35d20767fe97d369e7a84c99e7fd5e
Imobac Hauche Marín Cruz <imomarin@outlook.com>
2018-03-21 09:09:44 +0000

    Clase nº15: Router y componentes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,21 @@ import { routing, appRoutingProviders } from './app.routing';
 import { AppComponent } from './app.component';
 import { TiendaComponent } from './components/tienda/tienda.component';
 import { ParquesComponent } from './components/parques/parques.component';
+import { HomeComponent } from './components/home/home.component';
+import { AnimalsComponent } from './components/animals/animals.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { KeepersComponent } from './components/keepers/keepers.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TiendaComponent,
-    ParquesComponent
+    ParquesComponent,
+    HomeComponent,
+    AnimalsComponent,
+    ContactComponent,
+    KeepersComponent
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,12 +5,19 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { TiendaComponent } from './components/tienda/tienda.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AnimalsComponent } from './components/animals/animals.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { KeepersComponent } from './components/keepers/keepers.component';
 
 const appRoutes: Routes = [
-  { path: '', component: AppComponent },
-  // { path: '', redirectTo: 'tienda', pathMatch: 'full' },
-  { path: 'tienda', component: TiendaComponent },
-  { path: '**', component: TiendaComponent }
+  { path: '', component: HomeComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'animales', component: AnimalsComponent },
+  { path: 'contacto', component: ContactComponent },
+  { path: 'cuidadores', component: KeepersComponent },
+  { path: '**', component: HomeComponent }
 ];
 
 export const appRoutingProviders: any[] = [];
