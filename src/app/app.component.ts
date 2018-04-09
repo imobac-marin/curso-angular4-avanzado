b06c84e840cf7c655eefbcc0270e45ccb1940d77
Imobac Hauche Marín Cruz <imomarin@outlook.com>
2018-04-09 08:56:59 +0100

    Clase nº17: LocalStorage y DoCheck. Ejemplos prácticos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, DoCheck {
   title = 'Curso Angular 2-4-5 avanzado, MEAN, JWT, etc.';
+  emailContacto: string;
+
+  ngOnInit() {
+    console.log('Desde el localStorage: ' + localStorage.getItem('emailContacto'));
+  }
+
+  ngDoCheck() {
+    this.emailContacto = localStorage.getItem('emailContacto');
+  }
 }
