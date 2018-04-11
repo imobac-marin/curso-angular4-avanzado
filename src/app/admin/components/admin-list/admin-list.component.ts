8c7c9b7beabf53d7308f70f13c871a86fa2d049e
Imobac Hauche Marín Cruz <imomarin@outlook.com>
2018-04-11 08:56:12 +0100

    Clase nº31: Maquetación del módulo del panel de administración

--- src/app/admin/components/admin-list/admin-list.component.ts
+++ src/app/admin/components/admin-list/admin-list.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AdminListComponent implements OnInit {
 
   title = 'AdminList';
+  numbers = new Array(10);
   constructor() { }
 
   ngOnInit() {
