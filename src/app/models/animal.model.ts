479afc41a408edd298083d015aed3256f6b7ace7
Imobac Hauche Marín Cruz <imomarin@outlook.com>
2018-04-16 10:00:34 +0100

    Clase nº71: Crear modelos

---
+++ src/app/models/animal.model.ts
@@ -0,0 +1,15 @@
+import { User } from './user.model';
+
+export class Animal {
+
+  constructor(
+    public _id: string,
+    public name: string,
+    public description: string,
+    public year: string,
+    public image: string,
+    public user: string
+  ) {
+
+  }
+}
