966b1f3817dc881483ad905d3dd5b041abf6565a
Imobac Hauche Marín Cruz <imomarin@outlook.com>
2018-04-16 14:43:45 +0100

    Clase nº76: Vincular objeto de usuario al formulario de login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-login',
@@ -9,11 +10,18 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 export class LoginComponent implements OnInit {
 
   title = 'Identifícate';
+  public user: User;
 
-  constructor(private _router: Router, private _activatedRoute: ActivatedRoute) { }
+  constructor(private _router: Router, private _activatedRoute: ActivatedRoute) {
+    this.user = new User('', '', '', '', '', 'ROLE_USER', '');
+  }
 
   ngOnInit() {
     console.log('Se carga el componente Login');
+  }
+
+  onSubmit() {
+    console.log(this.user);
   }
 
 }
